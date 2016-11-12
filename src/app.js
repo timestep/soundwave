28bf46e17e8be0da633f9ac7b4aa7e539a7fbff2
Kevin Sivabalan <timestep@users.noreply.github.com>
2016-11-12 14:38:57 -0500

    Waveform (#2)
    * Found alternate Solutin
    
    * Ok

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('Hoopla')
+import WaveSurfer from 'wavesurfer.js';
+
+const audioFile = 'ringer.mp3';
+
+const configOptions = {
+  container: '#waveform',
+  scrollParent: true,
+};
+
+const waveSurfer = WaveSurfer
+  .create(configOptions);
+
+waveSurfer.load(audioFile);
+
+waveSurfer.on('ready', () => {
+  waveSurfer.play();
+});
+
+waveSurfer.on('finish', () => {
+  waveSurfer.play();
+});
